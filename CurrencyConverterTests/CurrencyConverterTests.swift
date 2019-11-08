b8d1f65fea766aa6aa35c4c86e2dd041436c74a7
MindaugasJucius <mindaugas.jucius@tesonet.com>
2019-11-08 14:33:18 +0200

    add CurrenciesModelController

--- CurrencyConverterTests/CurrencyConverterTests.swift
+++ CurrencyConverterTests/CurrencyConverterTests.swift
@@ -1,10 +1,3 @@
-//
-//  CurrencyConverterTests.swift
-//  CurrencyConverterTests
-//
-//  Created by Mindaugas Jucius on 08/11/2019.
-//
-
 import XCTest
 @testable import CurrencyConverter
 
@@ -18,16 +11,7 @@ class CurrencyConverterTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testCurrencyPairCreate() {
+        
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
