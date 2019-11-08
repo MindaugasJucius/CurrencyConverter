ceb40e2c5164d5bd5fb5693bfda149679542bc4c
MindaugasJucius <mindaugas.jucius@tesonet.com>
2019-11-08 14:24:53 +0200

    Initial Commit

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Mindaugas Jucius on 08/11/2019.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
