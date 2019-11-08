17f294f6ea7f90cbc80c9e1ce6c85b90bf5cc70c
MindaugasJucius <mindaugas.jucius@tesonet.com>
2019-11-08 16:13:15 +0200

    Limit CurrencyPair creation to CurrencyPairModelController

--- CurrencyConverter/Model/CurrencyPairModelController.swift
+++ CurrencyConverter/Model/CurrencyPairModelController.swift
@@ -1,14 +1,38 @@
 import Foundation
 
-struct CurrencyPair: Codable {
+struct CurrencyPair: Equatable {
     let baseCurrency: Currency
     let currencyToConvertTo: Currency
+    
+    /// _fileprivate_ forbids creation of _CurrencyPair_ not through _CurrencyPairModelController_
+    fileprivate init(baseCurrency: Currency,
+                     currencyToConvertTo: Currency) {
+        self.baseCurrency = baseCurrency
+        self.currencyToConvertTo = currencyToConvertTo
+    }
+    
+    var urlParameter: String {
+        return baseCurrency.identifier + currencyToConvertTo.identifier
+    }
 }
 
 class CurrencyPairModelController {
     
+    private let currencyPairArrayDefaultsKey = "persistedCurrencyPairs"
+    
     enum CurrencyPairError: Error {
         case equalIdentifiers
+    }
+    
+    var storedCurrencyPairs: [CurrencyPair] {
+        get {
+            let valueForKey = UserDefaults.standard.value(forKey: currencyPairArrayDefaultsKey)
+            guard let stored = valueForKey as? [CurrencyPair] else {
+                return []
+            }
+            
+            return stored
+        }
     }
     
     func createCurrencyPair(base: Currency, convertTo: Currency) throws -> CurrencyPair {
@@ -17,6 +41,11 @@ class CurrencyPairModelController {
         }
         
         return CurrencyPair.init(baseCurrency: base, currencyToConvertTo: convertTo)
+    }
+    
+    func storeCurrencyPair(currencyPair: CurrencyPair) {
+        var currentlyStoredPairs = storedCurrencyPairs
+        currentlyStoredPairs.append(currencyPair)
     }
     
 }
