7c1280dc5f71e39641d9bce2e9d7117c7171e3c3
MindaugasJucius <mindaugas.jucius@tesonet.com>
2019-11-08 14:57:59 +0200

    make loadCurrencies throwing

--- CurrencyConverter/Model/CurrenciesModelController.swift
+++ CurrencyConverter/Model/CurrenciesModelController.swift
@@ -1,13 +1,36 @@
 import Foundation
 
-struct Currency {
+struct Currency: Codable {
     let identifier: String
 }
 
 class CurrenciesModelController {
     
-    func loadCurrencies() -> [Currency] {
-        return []
+    enum CurrenciesError: Error {
+        case noResource
+        case decodeError(String)
+    }
+    
+    private let currenciesFileName = "currencies"
+    private let currenciesFileExtension = "json"
+
+    func loadCurrencies() throws -> [Currency] {
+        let resourceURL = Bundle.main.url(
+            forResource: currenciesFileName,
+            withExtension: currenciesFileExtension
+        )
+        
+        guard let currenciesFileURL = resourceURL else {
+            throw CurrenciesError.noResource
+        }
+        
+        do {
+            let currenciesFileData = try Data.init(contentsOf: currenciesFileURL)
+            let decodedCurrencyIdentifiers = try JSONDecoder().decode([String].self, from: currenciesFileData)
+            return decodedCurrencyIdentifiers.map(Currency.init)
+        } catch let error {
+            throw CurrenciesError.decodeError(error.localizedDescription)
+        }
     }
     
 }
