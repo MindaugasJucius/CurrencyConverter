b1362f6feed831b453680ec020fa4adc22183e61
MindaugasJucius <MindaugasJucius@icloud.com>
2019-11-13 20:20:53 +0200

    add currency pair cell

---
+++ CurrencyConverter/Currency Pair Observation/CurrencyPairTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  CurrencyPairTableViewCell.swift
+//  CurrencyConverter
+//
+//  Created by Mindaugas Jucius on 11/13/19.
+//
+
+import UIKit
+
+class CurrencyPairTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func update(pair: (CurrencyPair, Double)) {
+        
+    }
+    
+}
